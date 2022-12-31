7e86469c6f3139da33b080e8232a26fc1d66ccbd
Paul Dardeau <pauldardeau@gmail.com>
2022-12-31 15:57:24 -0600

    initial checkin

---
+++ Program.pas
@@ -0,0 +1,23 @@
+﻿namespace MacOxygeneCloudJukebox;
+
+uses
+  Foundation;
+
+type
+  Program = class
+  public
+
+    class method Main(args: array of String): Int32;
+    begin
+      var ArgList := new List<String>;
+      for each arg in args do begin
+        ArgList.Add(arg);
+      end;
+
+      var JBMain := new JukeboxMain;
+      result := JBMain.Run(ArgList);
+    end;
+
+  end;
+
+end.
